0b7006109a69b72ebc1b059cec2e1bb106e0cbb6
ATUL KUMAR <98544302+Atul245@users.noreply.github.com>
2023-06-19 18:06:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()  #instance of the app
+
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+
+    # only get published blogs
+
+    if published:
+        return {"data":f"{limit} published blogs from the database"}
+    else:
+        return {"data":f"{limit} blogs from the database"}
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return{"data":"all unpublished blogs"}
+
+@app.get('/blog/{id}')
+def show(id: int):
+
+    # fetch blog with id = id
+
+    return {"data" : id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+
+    # fetch comments of blog with id = id
+
+    return {"data":{"1","2"}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {"data":f"Blog is created with title as {blog.title}"}
